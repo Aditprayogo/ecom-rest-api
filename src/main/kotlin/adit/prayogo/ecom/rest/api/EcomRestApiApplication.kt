ed6960704097f2686f71722353923cb71dce47ca
aditPrayogo <adit.ihzar@gmail.com>
2020-10-10 17:26:26 +0700

    create API specs

---
+++ src/main/kotlin/adit/prayogo/ecom/rest/api/EcomRestApiApplication.kt
@@ -0,0 +1,11 @@
+package adit.prayogo.ecom.rest.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcomRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<EcomRestApiApplication>(*args)
+}
